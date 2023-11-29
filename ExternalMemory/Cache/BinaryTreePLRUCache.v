294c3279e350c35b88fabe459575d4ea61e91f88
Wevel <charlie.david.smith@hotmail.co.uk>
2023-11-29 22:02:49 +0000

    Fixed signal-wise circular logic. Bit-wise this was not an issue, but it caused a linter warning.

--- ExternalMemory/Cache/BinaryTreePLRUCache.v
+++ ExternalMemory/Cache/BinaryTreePLRUCache.v
@@ -27,15 +27,16 @@ generate
 			end
 		end
 
+		wire[PARENT_SIZE-1:0] lruAddressParent;
 		BinaryTreePLRUCache #(.SIZE(PARENT_SIZE)) parentLayer (
 			.clk(clk),
 			.rst(rst),
 			.enable(enable),
 			.address(address[SIZE-1:1]),
-			.lruAddress(lruAddress[SIZE-1:1]));
+			.lruAddress(lruAddressParent));
 
-		assign lruAddress[0] = state[lruAddress[SIZE-1:1]];
-		
+		assign lruAddress = { lruAddressParent, state[lruAddressParent] };
+
 	end else begin
 		always @(posedge clk) begin
 			if (rst) begin
